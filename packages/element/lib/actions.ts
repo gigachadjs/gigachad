4bfccefebb82865b8848b209395bc29b59d3d26c
Ethan Kircher <ethanmichaelk@gmail.com>
2024-06-15 23:27:46 -0500

    bind the method to the correct thing

--- packages/element/lib/actions.ts
+++ packages/element/lib/actions.ts
@@ -23,14 +23,16 @@ type ActionMethod = {
 
 export function addActionEventListener(raw: string, originalElement: Element, chadElement: ChadElement) {
   const { element, event, method, option } = decomposeAction(raw, originalElement);
+  const boundMethod = (chadElement as any)[method].bind(chadElement);
 
-  element.addEventListener(event, (chadElement as any)[method], decomposeOptionForAdd(option));
+  element.addEventListener(event, boundMethod, decomposeOptionForAdd(option));
 }
 
 export function removeActionEventListener(raw: string, originalElement: Element, chadElement: ChadElement) {
   const { element, event, method, option } = decomposeAction(raw, originalElement);
+  const boundMethod = (chadElement as any)[method].bind(chadElement);
 
-  element.removeEventListener(event, (chadElement as any)[method], decomposeOptionForRemove(option));
+  element.removeEventListener(event, boundMethod, decomposeOptionForRemove(option));
 }
 
 function decomposeAction(action: string, originalElement: Element): Action {
